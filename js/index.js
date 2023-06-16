5e5295f0a473fb95113dd3cb30d42293f6eb607b
fahimer <fahimermo@gmail.com>
2023-06-16 19:48:00 +0300

    refactor logical part

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
 let currentQuiz = 0
 let score = 0
-loadQuiz()
+
 function loadQuiz() {
     deselectAnswers()
     const currentQuizData = quizData[currentQuiz]
@@ -56,28 +56,27 @@ function deselectAnswers() {
     answerEls.forEach(answerEl => answerEl.checked = false)
 }
 function getSelected() {
-    let answer
-    answerEls.forEach(answerEl => {
-        if (answerEl.checked) {
-            answer = answerEl.id
-        }
-    })
-    return answer
+    let answer = document.querySelector(".answer:checked");
+
+    return answer ? answer.id : null
 }
 submitBtn.addEventListener('click', () => {
     const answer = getSelected()
     if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++
-        }
-        currentQuiz++
-        if (currentQuiz < quizData.length) {
-            loadQuiz()
-        } else {
-            quiz.innerHTML = `
+        return;
+    }
+    if (answer === quizData[currentQuiz].correct) {
+        score++
+    }
+    currentQuiz++
+    if (currentQuiz < quizData.length) {
+        loadQuiz()
+    } else {
+        quiz.innerHTML = `
            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
            <button onclick="location.reload()">Reload</button>
            `
-        }
     }
 })
+
+loadQuiz()
